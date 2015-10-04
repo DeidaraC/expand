097ee3302cc449a804db2a688d13a207387ac80e
JIANG Di <jiang-di@outlook.com>
2015-10-04 18:31:40 +0800

    it works

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@ static void print_usage(void);
 
 int main(int argc, const char *argv[]) {
   int tab_width = 8;
-  int c;
+  int c, i;
 
   if (argc < 2) {
     print_usage();
@@ -27,8 +27,25 @@ int main(int argc, const char *argv[]) {
 
   printf("tab_width: %d\n", tab_width);
 
-  for (size_t i = optind; i < argc; i++) {
-    printf("%s\n", argv[i]);
+  int ch;
+  FILE *fp;
+  for (i = optind; i < argc; i++) {
+    fp = fopen(argv[i], "r");
+    if (fp == NULL) {
+      fprintf(stderr, "can't open file %s\n", argv[i]);
+      exit(EXIT_FAILURE);
+    } else {
+      while ((ch = fgetc(fp)) != EOF) {
+        if (ch == '\t') {
+          for (size_t j = 0; j < tab_width; j++) {
+            putchar(' ');
+          }
+        } else {
+          putchar(ch);
+        }
+      }
+      fclose(fp);
+    }
   }
 
   return 0;
