461ae60e41b0f76279d718c51ec22d4a70207639
JIANG Di <jiang-di@outlook.com>
2015-10-11 20:28:33 +0800

    Add test for `tabs` option

--- test.rb
+++ test.rb
@@ -36,7 +36,19 @@ class TestExpand < Minitest::Test
     assert_equal "", stderr_str2
   end
 
-  def test_wrong_option
+  def test_option_tabs
+    stdout_str, stderr_str, status = Open3.capture3("./expand.out --tabs=6 test_text/text1.txt")
+    assert_equal IO.read("test_text/expanded_6_text1.txt"), stdout_str
+    assert_equal 0, status.exitstatus
+    assert_equal "", stderr_str
+
+    stdout_str2, stderr_str2, status2 = Open3.capture3("./expand.out --tabs=6 test_text/text3.txt")
+    assert_equal IO.read("test_text/expanded_6_text3.txt"), stdout_str2
+    assert_equal 0, status2.exitstatus
+    assert_equal "", stderr_str2
+  end
+
+  def test_wrong_option_t
     stdout_str5, stderr_str5, status5 = Open3.capture3("./expand.out")
     assert_equal "", stdout_str5
     assert_equal 1, status5.exitstatus
@@ -58,6 +70,33 @@ class TestExpand < Minitest::Test
     assert_equal "Usage: expand [-t number] [file ...]\n", stderr_str2
 
     stdout_str3, stderr_str3, status3 = Open3.capture3("./expand.out -t test_text/text1.txt")
+    assert_equal "", stdout_str3
+    assert_equal 1, status3.exitstatus
+    assert_equal "Usage: expand [-t number] [file ...]\n", stderr_str3
+
+    stdout_str4, stderr_str4, status4 = Open3.capture3("./expand.out -e test_text/text1.txt")
+    assert_equal "", stdout_str4
+    assert_equal 1, status4.exitstatus
+    assert_equal "./expand.out: invalid option -- 'e'\nUsage: expand [-t number] [file ...]\n", stderr_str4
+  end
+
+  def test_wrong_option_tabs
+    stdout_str6, stderr_str6, status6 = Open3.capture3("./expand.out --tabs")
+    assert_equal "", stdout_str6
+    assert_equal 1, status6.exitstatus
+    assert_equal "./expand.out: option '--tabs' requires an argument\nUsage: expand [-t number] [file ...]\n", stderr_str6
+
+    stdout_str, stderr_str, status = Open3.capture3("./expand.out -tabs=i test_text/text1.txt")
+    assert_equal "", stdout_str
+    assert_equal 1, status.exitstatus
+    assert_equal "Usage: expand [-t number] [file ...]\n", stderr_str
+
+    stdout_str2, stderr_str2, status2 = Open3.capture3("./expand.out --tabs=-1 test_text/text1.txt")
+    assert_equal "", stdout_str2
+    assert_equal 1, status2.exitstatus
+    assert_equal "Usage: expand [-t number] [file ...]\n", stderr_str2
+
+    stdout_str3, stderr_str3, status3 = Open3.capture3("./expand.out --tabs test_text/text1.txt")
     assert_equal "", stdout_str3
     assert_equal 1, status3.exitstatus
     assert_equal "Usage: expand [-t number] [file ...]\n", stderr_str3
